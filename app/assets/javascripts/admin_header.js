2b23e09265c3cfe31b233d52a16ffa7d20426988
winterwhisper <winterwhisper.dev@gmail.com>
2015-05-27 13:06:02 +0800

    重构后台模板

--- app/assets/javascripts/admin_header.js
+++ app/assets/javascripts/admin_header.js
@@ -10,10 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap
-//= require AdminLTE
-//= require jquery.slimscroll
-//= require fastclick
+//= require html5shiv
+//= require respond
