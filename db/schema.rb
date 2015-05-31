ba1e0e19728805b0dc65b424ed3d3ee1c332faa9
winterwhisper <winterwhisper.dev@gmail.com>
2015-05-31 20:19:11 +0800

    增加admin模型

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528034020) do
+ActiveRecord::Schema.define(version: 20150531121725) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "name",            null: false
+    t.string   "nickname",        null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "username"
