77f9c1ac6683f01a4809688c6344bbc6d58bb4b7
winterwhisper <winterwhisper.dev@gmail.com>
2015-06-05 11:46:44 +0800

    增加dashboard controller，并增加相关链接

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin, path: :console do
+    root to: 'dashboard#home'
     resources :posts, except: [:show]
     resources :comments, only: [:index, :destroy]
     resources :tags, except: [:show]
@@ -9,6 +10,10 @@ Rails.application.routes.draw do
       get :login, to: :new
       post :login, to: :create
       delete :logout, to: :destroy
+    end
+
+    controller :dashboard do
+      get :home, to: :home
     end
   end
 end
