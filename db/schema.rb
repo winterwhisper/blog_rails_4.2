5cc0511ecc9d0c8ca86af2446cf56e003afa8afb
winterwhisper <winterwhisper.dev@gmail.com>
2015-06-08 09:48:36 +0800

    admins表增加avatar字段，以存储头像路径

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531121725) do
+ActiveRecord::Schema.define(version: 20150608014747) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name",            null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150531121725) do
     t.string   "remember_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "avatar"
   end
 
   create_table "comments", force: :cascade do |t|
